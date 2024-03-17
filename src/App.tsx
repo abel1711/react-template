135442d56a441e961049925463623ebf81256f7f
Abel Aron Amieva <abelamieva@gmail.com>
2024-03-16 23:37:30 -0300

    webpack implemented

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,20 @@
 
 export const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <h1>App!!!</h1>
+      <button
+        style={{
+          backgroundColor: '#dadada',
+          border: 'none',
+          margin: 10,
+          padding: '10px 20px',
+          borderRadius: 10,
+          cursor: 'pointer'
+        }}
+      > Enviar </button>
+
+    </div>
   )
 }
 
